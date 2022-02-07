e646abf1ce87a37987456587d502b6f07cf4f9f1
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-02-08 05:50:14 +0700

    Adding splash screen.

--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/taufik/gitser/activity/MainActivity.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/taufik/gitser/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.taufik.gitser
+package com.taufik.gitser.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.taufik.gitser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
