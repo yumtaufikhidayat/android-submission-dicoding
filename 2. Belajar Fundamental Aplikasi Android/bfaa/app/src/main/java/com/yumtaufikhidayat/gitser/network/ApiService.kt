d9ad3f1d5e47d8b6a6a26cdd6ddcfc8e67095e01
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-10 05:59:44 +0700

    Add following, followers, and repository service.

--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/network/ApiService.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/network/ApiService.kt
@@ -1,12 +1,17 @@
 package com.yumtaufikhidayat.gitser.network
 
 import com.yumtaufikhidayat.gitser.data.response.detail.DetailResponse
+import com.yumtaufikhidayat.gitser.data.response.detail.RepositoryResponse
 import com.yumtaufikhidayat.gitser.data.response.search.Search
 import com.yumtaufikhidayat.gitser.data.response.search.SearchResponse
 import com.yumtaufikhidayat.gitser.network.UrlEndpoint.ALL_USERS
 import com.yumtaufikhidayat.gitser.network.UrlEndpoint.DETAIL_PROFILE_URL
+import com.yumtaufikhidayat.gitser.network.UrlEndpoint.FOLLOWERS_URL
+import com.yumtaufikhidayat.gitser.network.UrlEndpoint.FOLLOWING_URL
 import com.yumtaufikhidayat.gitser.network.UrlEndpoint.GITHUB_AUTH_TOKEN
+import com.yumtaufikhidayat.gitser.network.UrlEndpoint.REPOSITORY_URL
 import com.yumtaufikhidayat.gitser.network.UrlEndpoint.SEARCH_USERS
+import com.yumtaufikhidayat.gitser.network.UrlEndpoint.USERNAME
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -28,6 +33,24 @@ interface ApiService {
     @Headers(GITHUB_AUTH_TOKEN)
     @GET(DETAIL_PROFILE_URL)
     fun getDetailUser(
-        @Path("username") username: String
+        @Path(USERNAME) username: String
     ): Call<DetailResponse>
+
+    @Headers(GITHUB_AUTH_TOKEN)
+    @GET(FOLLOWING_URL)
+    fun getFollowingUsers(
+        @Path(USERNAME) username: String
+    ): Call<ArrayList<Search>>
+
+    @Headers(GITHUB_AUTH_TOKEN)
+    @GET(FOLLOWERS_URL)
+    fun getFollowerUsers(
+        @Path(USERNAME) username: String
+    ): Call<ArrayList<Search>>
+
+    @Headers(GITHUB_AUTH_TOKEN)
+    @GET(REPOSITORY_URL)
+    fun getRepositories(
+        @Path(USERNAME) username: String
+    ): Call<ArrayList<RepositoryResponse>>
 }
