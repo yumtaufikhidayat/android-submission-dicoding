7f7fd01215915a59a2e9f5038a0ff476840e07ef
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-10 06:53:07 +0700

    Add list of repositories.

--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/ui/fragment/RepositoryFragment.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/ui/fragment/RepositoryFragment.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yumtaufikhidayat.gitser.data.viewmodel.detail.DetailViewModel
 import com.yumtaufikhidayat.gitser.databinding.FragmentRepositoryBinding
+import com.yumtaufikhidayat.gitser.ui.activity.DetailActivity
+import com.yumtaufikhidayat.gitser.ui.adapter.RepositoryAdapter
 
 class RepositoryFragment : Fragment() {
 
     private var _binding: FragmentRepositoryBinding? = null
     private val binding get() = _binding!!
+
+    private val detailViewModel: DetailViewModel by viewModels()
+    private lateinit var repositoryAdapter: RepositoryAdapter
+    private lateinit var username: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +30,66 @@ class RepositoryFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getArgument()
+        initView()
+        showRepositoryData()
+    }
+
+    private fun getArgument() {
+        username = arguments?.getString(DetailActivity.EXTRA_PARCELABLE).toString()
+    }
+
+    private fun initView() {
+        repositoryAdapter = RepositoryAdapter()
+        binding.apply {
+            with(rvRepos) {
+                layoutManager = LinearLayoutManager(requireActivity())
+                adapter = repositoryAdapter
+            }
+        }
+    }
+
+    private fun showRepositoryData() {
+        showLoading(true)
+        detailViewModel.apply {
+            setListOfRepositories(username)
+            getListOfRepositories().observe(viewLifecycleOwner) {
+                if (it != null) {
+                    if (it.size != 0) {
+                        repositoryAdapter.submitList(it)
+                        showNoData(false)
+                    } else {
+                        showNoData(true)
+                    }
+                    showLoading(false)
+                }
+            }
+        }
+    }
+
+    private fun showLoading(isShow: Boolean) = with(binding){
+        if (isShow) {
+            shimmerLoading.visibility = View.VISIBLE
+            rvRepos.visibility = View.GONE
+        } else {
+            shimmerLoading.visibility = View.GONE
+            rvRepos.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showNoData(isShow: Boolean) = with(binding) {
+        if (isShow) {
+            viewNoRepositoryVisibility.visibility = View.VISIBLE
+        } else {
+            viewNoRepositoryVisibility.visibility = View.GONE
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
