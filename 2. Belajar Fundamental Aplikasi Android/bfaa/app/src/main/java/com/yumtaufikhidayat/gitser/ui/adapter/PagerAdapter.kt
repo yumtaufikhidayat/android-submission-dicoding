2967f178a9304a9fd0a1229c9786b77933dca88c
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-09 21:38:05 +0700

    Update PagerAdapter.kt class.

--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/ui/adapter/PagerAdapter.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/ui/adapter/PagerAdapter.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.yumtaufikhidayat.gitser.ui.fragment.FollowerFragment
+import com.yumtaufikhidayat.gitser.ui.fragment.FollowingFragment
+import com.yumtaufikhidayat.gitser.ui.fragment.RepositoryFragment
 
 class PagerAdapter(activity: AppCompatActivity, bundle: Bundle) : FragmentStateAdapter(activity) {
 
@@ -14,7 +17,9 @@ class PagerAdapter(activity: AppCompatActivity, bundle: Bundle) : FragmentStateA
     override fun createFragment(position: Int): Fragment {
         var fragment: Fragment? = null
         when (position) {
-
+            0 -> fragment = FollowingFragment()
+            1 -> fragment = FollowerFragment()
+            2 -> fragment = RepositoryFragment()
         }
         fragment?.arguments = this.bundleData
         return fragment as Fragment
