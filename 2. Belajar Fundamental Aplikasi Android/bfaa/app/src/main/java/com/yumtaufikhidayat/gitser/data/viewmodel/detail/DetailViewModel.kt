5cf89b0328cb5ba2998214e5721e46052ecb1666
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-10 17:11:23 +0700

    Remove coroutine in viewmodel.

--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/data/viewmodel/detail/DetailViewModel.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/data/viewmodel/detail/DetailViewModel.kt
@@ -8,9 +8,6 @@ import com.yumtaufikhidayat.gitser.data.response.detail.DetailResponse
 import com.yumtaufikhidayat.gitser.data.response.detail.RepositoryResponse
 import com.yumtaufikhidayat.gitser.data.response.search.Search
 import com.yumtaufikhidayat.gitser.network.ApiConfig
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,92 +21,84 @@ class DetailViewModel: ViewModel() {
     private val listOfRepositories = MutableLiveData<ArrayList<RepositoryResponse>>()
 
     fun setDetailData(username: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.getDetailUser(username)
-                .enqueue(object : Callback<DetailResponse>{
-                    override fun onResponse(
-                        call: Call<DetailResponse>,
-                        response: Response<DetailResponse>
-                    ) {
-                        if (response.isSuccessful) {
-                            detailData.postValue(response.body())
-                        }
+        apiConfig.getDetailUser(username)
+            .enqueue(object : Callback<DetailResponse>{
+                override fun onResponse(
+                    call: Call<DetailResponse>,
+                    response: Response<DetailResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        detailData.postValue(response.body())
                     }
+                }
 
-                    override fun onFailure(call: Call<DetailResponse>, t: Throwable) {
-                        Log.d(TAG, "onFailure: ${t.message}")
-                    }
-                })
-        }
+                override fun onFailure(call: Call<DetailResponse>, t: Throwable) {
+                    Log.d(TAG, "onFailure: ${t.message}")
+                }
+            })
     }
 
     fun getDetailData(): LiveData<DetailResponse> = detailData
 
     fun setListOfFollowing(username: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.getFollowingUsers(username)
-                .enqueue(object : Callback<ArrayList<Search>>{
-                    override fun onResponse(
-                        call: Call<ArrayList<Search>>,
-                        response: Response<ArrayList<Search>>
-                    ) {
-                        if (response.isSuccessful) {
-                            listOfFollowing.postValue(response.body())
-                        }
+        apiConfig.getFollowingUsers(username)
+            .enqueue(object : Callback<ArrayList<Search>>{
+                override fun onResponse(
+                    call: Call<ArrayList<Search>>,
+                    response: Response<ArrayList<Search>>
+                ) {
+                    if (response.isSuccessful) {
+                        listOfFollowing.postValue(response.body())
                     }
+                }
 
-                    override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
-                        Log.e(TAG, "onFailure: ${t.message}")
-                    }
-                })
-        }
+                override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
+                    Log.e(TAG, "onFailure: ${t.message}")
+                }
+            })
     }
 
     fun getListOfFollowing(): LiveData<ArrayList<Search>> = listOfFollowing
 
     fun setListOfFollower(username: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.getFollowerUsers(username)
-                .enqueue(object : Callback<ArrayList<Search>>{
-                    override fun onResponse(
-                        call: Call<ArrayList<Search>>,
-                        response: Response<ArrayList<Search>>
-                    ) {
-                        if (response.isSuccessful) {
-                            listOfFollowers.postValue(response.body())
-                        }
+        apiConfig.getFollowerUsers(username)
+            .enqueue(object : Callback<ArrayList<Search>>{
+                override fun onResponse(
+                    call: Call<ArrayList<Search>>,
+                    response: Response<ArrayList<Search>>
+                ) {
+                    if (response.isSuccessful) {
+                        listOfFollowers.postValue(response.body())
                     }
+                }
 
-                    override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
-                        Log.e(TAG, "onFailure: ${t.message}")
-                    }
-                })
-        }
+                override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
+                    Log.e(TAG, "onFailure: ${t.message}")
+                }
+            })
     }
 
     fun getListOfFollower(): LiveData<ArrayList<Search>> = listOfFollowers
 
     fun setListOfRepositories(username: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.getRepositories(username)
-                .enqueue(object : Callback<ArrayList<RepositoryResponse>>{
-                    override fun onResponse(
-                        call: Call<ArrayList<RepositoryResponse>>,
-                        response: Response<ArrayList<RepositoryResponse>>
-                    ) {
-                        if (response.isSuccessful) {
-                            listOfRepositories.postValue(response.body())
-                        }
+        apiConfig.getRepositories(username)
+            .enqueue(object : Callback<ArrayList<RepositoryResponse>>{
+                override fun onResponse(
+                    call: Call<ArrayList<RepositoryResponse>>,
+                    response: Response<ArrayList<RepositoryResponse>>
+                ) {
+                    if (response.isSuccessful) {
+                        listOfRepositories.postValue(response.body())
                     }
+                }
 
-                    override fun onFailure(
-                        call: Call<ArrayList<RepositoryResponse>>,
-                        t: Throwable
-                    ) {
-                        Log.e(TAG, "onFailure: ${t.message}")
-                    }
-                })
-        }
+                override fun onFailure(
+                    call: Call<ArrayList<RepositoryResponse>>,
+                    t: Throwable
+                ) {
+                    Log.e(TAG, "onFailure: ${t.message}")
+                }
+            })
     }
 
     fun getListOfRepositories(): LiveData<ArrayList<RepositoryResponse>> = listOfRepositories
--- 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/data/viewmodel/main/MainViewModel.kt
+++ 2. Belajar Fundamental Aplikasi Android/bfaa/app/src/main/java/com/yumtaufikhidayat/gitser/data/viewmodel/main/MainViewModel.kt
@@ -7,9 +7,6 @@ import androidx.lifecycle.ViewModel
 import com.yumtaufikhidayat.gitser.data.response.search.Search
 import com.yumtaufikhidayat.gitser.data.response.search.SearchResponse
 import com.yumtaufikhidayat.gitser.network.ApiConfig
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,43 +17,39 @@ class MainViewModel: ViewModel() {
     private val listAllUsers = MutableLiveData<ArrayList<Search>>()
 
     fun setAllUsers() {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.getAllUsers().enqueue(object : Callback<ArrayList<Search>>{
-                override fun onResponse(
-                    call: Call<ArrayList<Search>>,
-                    response: Response<ArrayList<Search>>
-                ) {
-                    if (response.isSuccessful) {
-                        listAllUsers.postValue(response.body())
-                    }
+        apiConfig.getAllUsers().enqueue(object : Callback<ArrayList<Search>>{
+            override fun onResponse(
+                call: Call<ArrayList<Search>>,
+                response: Response<ArrayList<Search>>
+            ) {
+                if (response.isSuccessful) {
+                    listAllUsers.postValue(response.body())
                 }
+            }
 
-                override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
-                    Log.e(TAG, "onFailure: ${t.message}")
-                }
-            })
-        }
+            override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
     }
 
     fun getAllUsers(): LiveData<ArrayList<Search>> = listAllUsers
 
     fun setSearchUser(query: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            apiConfig.searchUser(query).enqueue(object : Callback<SearchResponse>{
-                override fun onResponse(
-                    call: Call<SearchResponse>,
-                    response: Response<SearchResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        listAllUsers.postValue(response.body()?.items)
-                    }
+        apiConfig.searchUser(query).enqueue(object : Callback<SearchResponse>{
+            override fun onResponse(
+                call: Call<SearchResponse>,
+                response: Response<SearchResponse>
+            ) {
+                if (response.isSuccessful) {
+                    listAllUsers.postValue(response.body()?.items)
                 }
+            }
 
-                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                    Log.e(TAG, "onFailure: ${t.message}")
-                }
-            })
-        }
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
     }
 
     fun getSearchUser(): LiveData<ArrayList<Search>> = listAllUsers
